29c1306153855bdb924d1ef8d9aeae182363a3ab
Sami Habbaba <sami.software@hotmail.com>
2021-12-16 09:38:01 +0200

    starting view bets

--- src/app/bets/view-bets/view-bets.component.ts
+++ src/app/bets/view-bets/view-bets.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-view-bets',
   templateUrl: './view-bets.component.html',
-  styleUrls: ['./view-bets.component.css']
+  styleUrls: ['./view-bets.component.scss']
 })
 export class ViewBetsComponent implements OnInit {
 
